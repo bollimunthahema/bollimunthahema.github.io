0cff69468baf4df6dea6db66ae1e6c31420cd525
bollimunthahema <hemabollimuntha07@gmail.com>
2026-03-03 09:39:36 +0530

    Update script.js

--- digital_clock/script.js
+++ digital_clock/script.js
@@ -1,1 +1,32 @@
+let hrs = document.querySelector('#hrs');
+let mins= document.querySelector('#min');
+let sec = document.querySelector('#sec');
+
+
+// let currentTime = new Date();
+
+// console.log(currentTime.getHours());
+
+
+// hrs.innerHTML = currentTime.getHours();
+
+// mins.innerHTML = currentTime.getMinutes();
+
+// sec.innerHTML = currentTime.getSeconds();
+
+
+setInterval(()=>{
+	
+	let currentTime = new Date();
+
+	// console.log((currentTime.getSeconds()<10?"0":"")+currentTime.getSeconds());
+
+hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
+
+mins.innerHTML = (currentTime.getMinutes()<10?"":"") + currentTime.getMinutes();
+
+sec.innerHTML = (currentTime.getSeconds() <10 ? "0":"") + currentTime.getSeconds();
+
+},1000)
+
 
